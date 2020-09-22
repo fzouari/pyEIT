b98598ac3c75f1d8fc3b32449be0a044ef5b34d1
Fedi ZOUARI <fzouari@users.noreply.github.com>
2020-09-22 12:19:17 +0800

    Reverted the change in original jac to make it similar to the original

--- pyeit/eit/jac.py
+++ pyeit/eit/jac.py
@@ -9,7 +9,6 @@ from __future__ import division, absolute_import, print_function
 
 import numpy as np
 import scipy.linalg as la
-from numpy.linalg import multi_dot
 
 from .base import EitBase
 
@@ -17,7 +16,7 @@ from .base import EitBase
 class JAC(EitBase):
     """ A sensitivity-based EIT imaging class """
 
-    def setup(self, p=0.20, lamb=0.001, method='kotre', W=None, Wx=None, xp=None):
+    def setup(self, p=0.20, lamb=0.001, method='kotre'):
         """
         JAC, default file parser is 'std'
 
@@ -28,30 +27,15 @@ class JAC(EitBase):
         method: str
             regularization methods
         """
-        jac = self.J
-        if W is None:
-            W = np.eye(jac.shape[0])
-        if Wx is None:
-            Wx = np.eye(jac.shape[1])
-        if xp is None:
-            xp = np.zeros((jac.shape[1]))
-
         # passing imaging parameters
         self.params = {
             'p': p,
             'lamb': lamb,
-            'method': method,
-            'W': W,
-            'Wx': Wx,
-            'xp': xp
+            'method': method
         }
         # pre-compute H0 for dynamical imaging
         # H = (J.T*J + R)^(-1) * J.T
-        self.H = h_matrix(self.J, p, lamb, method, W)
-        # pre-compute Q for dynamical imaging
-        # Q = (J.T*W*J + R)^(-1)
-        self.Q, r_mat = q_matrix(self.J, p, lamb, W, Wx, method=method)
-        self.params['Wx'] = np.dot(r_mat,Wx)
+        self.H = h_matrix(self.J, p, lamb, method)
 
     def solve(self, v1, v0, normalize=False):
         """ dynamic solve_eit
@@ -75,54 +59,8 @@ class JAC(EitBase):
         else:
             dv = (v1 - v0)
         # s = -Hv
-
-        jac = self.J
-        h_mat = multi_dot([self.Q, jac.transpose(), self.params['W']])
-        p_mat = np.dot(self.Q, self.params['Wx'])
-        ds = -np.dot(h_mat, dv) + self.params['lamb']*np.dot(p_mat, self.params['xp'])
-
-        # ds = -np.dot(la.inv(j_w_j + lamb * r_mat), jac.transpose())
-
+        ds = -np.dot(self.H, dv)
         return ds
-
-    def solve_P(self, v1, v0, normalize=False):
-        """ dynamic solve_eit
-
-        Parameters
-        ----------
-        v1: NDArray
-            current frame
-        v0: NDArray, optional
-            referenced frame, d = H(v1 - v0)
-        normalize: Boolean
-            true for conducting normalization
-
-        Returns
-        -------
-        ds: NDArray
-            complex-valued NDArray, changes of conductivities
-        """
-        if normalize:
-            dv = self.normalize(v1, v0)
-        else:
-            dv = (v1 - v0)
-        # s = -Hv
-
-        jac = self.J
-        h_mat = multi_dot([self.Q, jac.transpose(), self.params['W']])
-        p_mat = np.dot(self.Q, self.params['Wx'])
-
-        ds = -np.dot(h_mat, dv) + self.params['lamb']*np.dot(p_mat, self.params['xp'])
-
-        Ax_minus_y = np.dot(-jac, ds) - dv
-        x_minus_xp = ds - self.params['xp']
-
-        xi = np.log10(multi_dot([np.conjugate(Ax_minus_y).transpose(), self.params['W'], Ax_minus_y]))
-        eta = np.log10(multi_dot([np.conjugate(x_minus_xp).transpose(), self.params['Wx'], x_minus_xp]))
-        # xi = multi_dot([np.conjugate(Ax_minus_y).transpose(), self.params['W'], Ax_minus_y])
-        # eta = multi_dot([np.conjugate(x_minus_xp).transpose(), self.params['Wx'], x_minus_xp])
-
-        return (xi, eta)
 
     def map(self, v):
         """ return Hv """
@@ -174,7 +112,7 @@ class JAC(EitBase):
         p, lamb: float
             JAC parameters (can be overridden)
         lamb_decay: float
-            decay of lamb0, i.e., lamb0 = lamb0 * lamb_delay of each iteration
+            decay of lamb0, i.e., lamb0 = lamb0 * lamb_decay of each iteration
         lamb_min: float
             minimal value of lamb
         method: str, optional
@@ -195,6 +133,8 @@ class JAC(EitBase):
             R = diag(J^TJ)**p
             r0 (residual) = real_measure - forward_v
         """
+        from sklearn.metrics import r2_score
+
         if x0 is None:
             x0 = self.perm
         if p is None:
@@ -206,7 +146,8 @@ class JAC(EitBase):
 
         # convergence test
         x0_norm = np.linalg.norm(x0)
-
+        convergence = []
+        r2i = []
         for i in range(maxiter):
 
             # forward solver
@@ -225,17 +166,23 @@ class JAC(EitBase):
 
             # convergence test
             c = np.linalg.norm(d_k) / x0_norm
+            r2 = r2_score(v, fs.v)
+            convergence.append(c)
+            r2i.append(r2)
+
             if c < gtol:
                 break
 
             if verbose:
-                print('iter = %d, lamb = %f, gtol = %f' % (i, lamb, c))
+                print('iter = %d, lamb = %f, gtol = %f, r2 = %f' % (i, lamb, c, r2))
 
             # update regularization parameter
             # TODO: support user defined decreasing order of lambda series
             lamb *= lamb_decay
             if lamb < lamb_min:
                 lamb = lamb_min
+
+        self.tol = {'convergence': convergence, 'r2': r2i}
 
         return x0
 
@@ -255,47 +202,7 @@ class JAC(EitBase):
         return np.dot(d_mat, ds)
 
 
-def q_matrix(jac, p, lamb, W, Wx, method='kotre'):
-    """
-    JAC method of dynamic EIT solver:
-        Q = (J.T*W*J + lamb*Wx)^(-1)
-
-    Parameters
-    ----------
-    jac: NDArray
-        Jacobian
-    p, lamb: float
-        regularization parameters
-    method: str, optional
-        regularization method
-
-    Returns
-    -------
-    H: NDArray
-        pseudo-inverse matrix of JAC
-    """
-    j_w_j = multi_dot([jac.transpose(), W, jac])
-
-    if method == 'kotre':
-        # see adler-dai-lionheart-2007
-        # p=0   : noise distribute on the boundary ('dgn')
-        # p=0.5 : noise distribute on the middle
-        # p=1   : noise distribute on the center ('lm')
-        r_mat = np.diag(np.diag(j_w_j)) ** p
-    elif method == 'lm':
-        # Marquardt–Levenberg, 'lm' for short
-        # or can be called NOSER, DLS
-        r_mat = np.diag(np.diag(j_w_j))
-    else:
-        # Damped Gauss Newton, 'dgn' for short
-        r_mat = np.eye(jac.shape[1])
-
-    # build Q
-    q_mat = la.inv(j_w_j + lamb*np.dot(r_mat, Wx))
-    return q_mat, r_mat
-
-
-def h_matrix(jac, p, lamb, method='kotre', W=None):
+def h_matrix(jac, p, lamb, method='kotre'):
     """
     JAC method of dynamic EIT solver:
         H = (J.T*J + lamb*R)^(-1) * J.T
@@ -314,11 +221,7 @@ def h_matrix(jac, p, lamb, method='kotre', W=None):
     H: NDArray
         pseudo-inverse matrix of JAC
     """
-    if W is None:
-        j_w_j = np.dot(jac.transpose(), jac)
-    else:
-        j_w_j = multi_dot([jac.transpose(), W, jac])
-
+    j_w_j = np.dot(jac.transpose(), jac)
     if method == 'kotre':
         # see adler-dai-lionheart-2007
         # p=0   : noise distribute on the boundary ('dgn')
@@ -335,7 +238,6 @@ def h_matrix(jac, p, lamb, method='kotre', W=None):
 
     # build H
     h_mat = np.dot(la.inv(j_w_j + lamb*r_mat), jac.transpose())
-
     return h_mat
 
 
